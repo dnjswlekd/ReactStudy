96bd61b24e87ddf05dcdc9bdad29f8fd3541725a
dnjswlekd <wj8014@gmail.com>
2023-10-09 11:51:18 +0900

    let과 const 적절한 사용법
    let- 연산자로 새값 할당 가능, 타이핑 적음
    const - 새값 할당 불가능, 의도 명확

--- javascript-refresher/src/App.js
+++ javascript-refresher/src/App.js
@@ -1,8 +1,15 @@
-// import { apiKey } from './util.js';
-// import apiKey from './util.js';
-import { apiKey, abc as saehoon } from './util.js';
-// import * as util from './util.js'
+// // import { apiKey } from './util.js';
+// // import apiKey from './util.js';
+// import { apiKey, abc as saehoon } from './util.js';
+// // import * as util from './util.js'
 
 
-// console.log (util.apiKey);
-console.log(saehoon)
+// // console.log (util.apiKey);
+// console.log(saehoon)
+
+let userMessage = 'Hello World!';
+// const userMessage = 'Hello World!';
+
+userMessage = 'wonji'
+
+console.log(userMessage)
