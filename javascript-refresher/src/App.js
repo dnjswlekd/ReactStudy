b1a2b32013812cf607d6df6f258c0a4831297487
dnjswlekd <wj8014@gmail.com>
2023-10-09 16:05:55 +0900

    연산자 기본 복습하기

--- javascript-refresher/src/App.js
+++ javascript-refresher/src/App.js
@@ -3,13 +3,18 @@
 // import { apiKey, abc as saehoon } from './util.js';
 // // import * as util from './util.js'
 
-
 // // console.log (util.apiKey);
 // console.log(saehoon)
 
-let userMessage = 'Hello World!';
-// const userMessage = 'Hello World!';
+// let userMessage = 'Hello World!';
+// // const userMessage = 'Hello World!';
 
-userMessage = 'wonji'
+// userMessage = 'wonji'
 
-console.log(userMessage)
+// console.log(userMessage);
+console.log(10 + 5);
+console.log('hello'+'world!');
+
+if(10 == 10) {
+  console.log('wonji'+'sahoon')
+}
